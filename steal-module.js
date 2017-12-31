dc00cc67fcc34ae8f59c576013095ef348ee2779
Frank Lemanschik <frank@github.dspeed.eu>
2017-12-31 21:46:44 +0000

    Create steal-module.js

---
+++ steal-module.js
@@ -0,0 +1,27 @@
+class STEALComponent extends HTMLElement {
+  constructor () {
+    super()
+
+    // create a shadow root
+    this.shadow = this.attachShadow({ mode: 'open' })
+  }
+  connectedCallback () {
+          // build a <template> element to inject
+          const template = document.createElement('template')
+
+          // add the JS last, in a <script> tag, as well as wrapped by an IIFE
+          // the IIFE ensures nothing leaks to the window
+          template.innerHTML += `
+            <script>
+              (() => {
+                ${this.innerHTML}
+              })()
+            </script>
+         `
+          // inject our DOM content into the cached Shadow DOM
+          // this is what is required to make the JS execute
+          this.shadow.appendChild(document.importNode(template.content, true))
+  }
+}
+
+customElements.define('steal-module', STEALComponent)
